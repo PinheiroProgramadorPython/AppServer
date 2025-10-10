f9dbff72e8f5501a7a80dd42fdb98d6de7120fa0
PinheiroProgramadorPython <pinheiroprogramadorpython@gmail.com>
2025-10-09 21:00:21 -0300

    Ajustes com Uploads de Fotos

---
+++ config/cloudinary.js
@@ -0,0 +1,9 @@
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+    cloud_name: "dqk3aidxd",
+    api_key: "224177982232739",
+    api_secret: "TBKpQjkY7N3kYGZI67hUyk7zi5I",
+});
+
+export default cloudinary;
