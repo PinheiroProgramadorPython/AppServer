cb9fe3c10b9a049c2bb41b20cd69db33dea1a924
PinheiroProgramadorPython <pinheiroprogramadorpython@gmail.com>
2025-10-11 20:14:20 -0300

    Update app_server.js

--- app_server.js
+++ app_server.js
@@ -23,7 +23,7 @@ connectMongoDB();
 const app = express()
 
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: "*",
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ["Content-Type"]
 }));
