fec9c5397e0383b0eb72389e67998cd275b7e9e7
PinheiroProgramadorPython <pinheiroprogramadorpython@gmail.com>
2025-10-12 08:06:44 -0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -4,36 +4,36 @@ import userSchema from "../models/usuario.js";
 
 const router = express.Router();
 
-router.get("/usuarios", (req, resp) => {
+router.get("/usuarios", async (req, resp) => {
     try {
-        const usuarios = userSchema.find();
+        const usuarios = await userSchema.find();
         resp.status(200).json(usuarios);
     } catch (error) {
         resp.json({ error: error, message: "Request Failure" }).status(400);
     }
 });
 
-router.post("/usuarios", (req, resp) => {
+router.post("/usuarios", async (req, resp) => {
     try {
-        const usuario = userSchema.create(req.body);
+        const usuario = await userSchema.create(req.body);
         resp.status(200).json(usuario);
     } catch (error) {
         resp.json({ error: error, message: "Request Failure" }).status(400);
     }
 });
 
-router.put("/usuarios/:id", (req, resp) => {
+router.put("/usuarios/:id", async (req, resp) => {
     try {
-        const usuario = userSchema.findOneAndUpdate({ id: req.params.id }, req.body, { new: true });
+        const usuario = await userSchema.findOneAndUpdate({ id: req.params.id }, req.body, { new: true });
         resp.status(200).json(usuario);
     } catch (error) {
         resp.json({ error: error, message: "Request Failure" }).status(400);
     }
 });
 
-router.delete("/usuarios/:id", (req, resp) => {
+router.delete("/usuarios/:id", async (req, resp) => {
     try {
-        const usuario = userSchema.findOneAndDelete({ id: req.params.id });
+        const usuario = await userSchema.findOneAndDelete({ id: req.params.id });
         resp.status(200).json(usuario);
     } catch (error) {
         resp.json({ error: error, message: "Request Failure" }).status(400);
